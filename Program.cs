ca38850fe1280b525d51ee2010d84ae6f6e36d08
Jaxon Panter <jmanpanter@gmail.com>
2023-07-19 13:16:26 -0400

    Variables Complete

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//Delcaring and Initializing variables
+string name = "Jaxon";
+int age = 18;
+char middleInitial = 'R';
+bool isOver15 = true;
+double currentTemp = 92.3;
+decimal currentPrice = 25.99m;
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 15: {isOver15}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
